4b3aa4c958abc802c6f2785dc82992bd919495a0
Crazydoginal <rileyhadadream@gmail.com>
2026-01-17 18:18:24 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,5 +4,4 @@ import App from "./App.jsx"
 import "./index.css"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-React.createElement(React.StrictMode, null, React.createElement(App, null))
-)
+React.createElement(React.StrictMode, null, React.createElement(App, null)))
